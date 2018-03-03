a71ac278b1422c0cea34d01ddabdbb9aa5e60c9c
Epalle <cyrille.epalle@epita.fr>
2018-03-03 08:23:39 +0100

    Mcude_water_handler change

--- js/Mcube_handler.js
+++ js/Mcube_handler.js
@@ -1,22 +1,20 @@
+let t = 1;
 let c = 1;
 let h = 1;
-let t = 1;
+let s = 0.99;
 
 function Mcube_water_handler(Mcube_water, U, V) {
   for (i = 0; i < Mcube_water.length; i++) {
     for (j = 0; j < Mcube_water[i].length; j++) {
-      let tot_h = 0; // change bordr?
+      let tot_h = 0;
       tot_h += (i - 1 >= 0 ? U[i - 1][j] : U[i][j]);
       tot_h += (i + 1 < U.length ? U[i + 1][j] : U[i][j]);
       tot_h += (j - 1 >= 0 ? U[i][j - 1] : U[i][j]);
       tot_h += (j + 1 < U[i].length ? U[i][j + 1] : U[i][j]);
-      V[i][j] += c ^ 2(tot_h - 4 * U[i][j]);
-      if (V[i][j] < -1) {
-        V[i][j] = -1;
-      } else if (V[i][j] > 1) {
-        V[i][j] = 1;
-      }
-      Mcube_water[i][j].position.y += V[i][j];
+      let f = (tot_h - 4 * U[i][j]) * c * c / (h * h)
+      V[i][j] += f * t;
+      V[i][j] *= s;
+      Mcube_water[i][j].position.y += V[i][j] * t;
     }
   }
   for (i = 0; i < Mcube_water.length; i++) {
