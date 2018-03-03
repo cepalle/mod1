9635b3f5c423ec572c55f1e30ff0786685f0d3a4
Epalle <cyrille.epalle@epita.fr>
2018-03-03 22:59:20 +0100

    triangle with shadow add

--- js/scene_handler.js
+++ js/scene_handler.js
@@ -1,4 +1,4 @@
-const scene_sqaure = 2;
+const scene_sqaure = 250;
 
 const materialVert = new THREE.MeshLambertMaterial({
   color: 0x00ff00,
@@ -12,7 +12,7 @@ const materialBleau = new THREE.MeshBasicMaterial({
 let U = [];
 let V = [];
 let S = [];
-let geometry = new THREE.BufferGeometry();
+//let geometry = new THREE.BufferGeometry();
 
 init();
 
@@ -34,57 +34,92 @@ function scene_init(scene) {
   scene.add(light);
 
 
-  // create a simple square shape. We duplicate the top left and bottom right
-  // vertices because each vertex needs to appear once per triangle.
-
-  let positions = new Float32Array([
-    0, 0, 0,
-    1, 0, 0,
-    1, 0, 1,
-
-    0, 0, 0,
-    0, 0, 1,
-    1, 0, 1,
-  ]);
-
-  geometry.addAttribute('position', new THREE.BufferAttribute(positions, 3));
-
-
-
-
-
-
-
-
-
-  let normals = [];
-  for (let i = 0; i <= scene_sqaure; i++) {
-    for (let j = 0; j <= scene_sqaure; j++) {
-      normals.push(0, 0, 1);
-    }
+  var triangles = 500000;
+  var geometry = new THREE.BufferGeometry();
+  var positions = [];
+  var normals = [];
+  var colors = [];
+  var color = new THREE.Color();
+  var n = 800,
+    n2 = n / 2; // triangles spread in the cube
+  var d = 12,
+    d2 = d / 2; // individual triangle size
+  var pA = new THREE.Vector3();
+  var pB = new THREE.Vector3();
+  var pC = new THREE.Vector3();
+  var cb = new THREE.Vector3();
+  var ab = new THREE.Vector3();
+  for (var i = 0; i < triangles; i++) {
+    // positions
+    var x = Math.random() * n - n2;
+    var y = Math.random() * n - n2;
+    var z = Math.random() * n - n2;
+    var ax = x + Math.random() * d - d2;
+    var ay = y + Math.random() * d - d2;
+    var az = z + Math.random() * d - d2;
+    var bx = x + Math.random() * d - d2;
+    var by = y + Math.random() * d - d2;
+    var bz = z + Math.random() * d - d2;
+    var cx = x + Math.random() * d - d2;
+    var cy = y + Math.random() * d - d2;
+    var cz = z + Math.random() * d - d2;
+    positions.push(ax, ay, az);
+    positions.push(bx, by, bz);
+    positions.push(cx, cy, cz);
+    // flat face normals
+    pA.set(ax, ay, az);
+    pB.set(bx, by, bz);
+    pC.set(cx, cy, cz);
+    cb.subVectors(pC, pB);
+    ab.subVectors(pA, pB);
+    cb.cross(ab);
+    cb.normalize();
+    var nx = cb.x;
+    var ny = cb.y;
+    var nz = cb.z;
+    normals.push(nx * 32767, ny * 32767, nz * 32767);
+    normals.push(nx * 32767, ny * 32767, nz * 32767);
+    normals.push(nx * 32767, ny * 32767, nz * 32767);
+    // colors
+    var vx = (x / n) + 0.5;
+    var vy = (y / n) + 0.5;
+    var vz = (z / n) + 0.5;
+    color.setRGB(vx, vy, vz);
+    colors.push(color.r * 255, color.g * 255, color.b * 255);
+    colors.push(color.r * 255, color.g * 255, color.b * 255);
+    colors.push(color.r * 255, color.g * 255, color.b * 255);
   }
-  geometry.addAttribute('normal', new THREE.Float32BufferAttribute(normals, 3));
-
-
-
-
-
-
-
-
-
-  geometry.attributes.position.needsUpdate = true;
-  let mesh = new THREE.Mesh(geometry, materialBleau);
+  var positionAttribute = new THREE.Float32BufferAttribute(positions, 3);
+  var normalAttribute = new THREE.Int16BufferAttribute(normals, 3);
+  var colorAttribute = new THREE.Uint8BufferAttribute(colors, 3);
+  normalAttribute.normalized = true; // this will map the buffer values to 0.0f - +1.0f in the shader
+  colorAttribute.normalized = true;
+  geometry.addAttribute('position', positionAttribute);
+  geometry.addAttribute('normal', normalAttribute);
+  geometry.addAttribute('color', colorAttribute);
+  geometry.computeBoundingSphere();
+  var material = new THREE.MeshLambertMaterial({
+    color: 0xaaaaaa,
+    side: THREE.DoubleSide,
+    vertexColors: THREE.VertexColors
+  });
+  mesh = new THREE.Mesh(geometry, material);
   scene.add(mesh);
+
+
+
+
+
+
 }
 
 function positions_upadte() {
-  let positions = geometry.attributes.position.array;
-  positions[17] = 2;
+  //let positions = geometry.attributes.position.array;
+  //positions[17] = 2;
 }
 
 function scene_update(scene) {
   water_update(U, V, S);
-  positions_upadte();
-  geometry.attributes.position.needsUpdate = true;
+  //positions_upadte();
+  //geometry.attributes.position.needsUpdate = true;
 }
