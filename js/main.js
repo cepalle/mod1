b2294f23b919b5a2f98f2db41d3720910fc55d62
Epalle <cyrille.epalle@epita.fr>
2018-03-03 16:38:49 +0100

    change camera pos

--- js/main.js
+++ js/main.js
@@ -1,13 +1,12 @@
 const dim_view = 32;
 
-
 let renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-let camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 1, 500);
+let camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 500);
 camera.position.set(dim_view * 1.5, dim_view, dim_view * 1.5);
-camera.lookAt(new THREE.Vector3(dim_view / 2, 0, dim_view / 2));
+camera.lookAt(new THREE.Vector3(dim_view, 0, dim_view));
 
 let scene = new THREE.Scene();
 
