d12d86209882f45a8e4e0a54ac4db1593813e9b5
Epalle <cyrille.epalle@epita.fr>
2018-03-05 10:35:09 +0100

    add stats and start gui

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+const stats = new Stats();
+stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
+document.body.appendChild(stats.dom);
+
 const renderer = new THREE.WebGLRenderer();
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -8,15 +12,30 @@ camera.position.set(scene_res * 1.5, scene_res * 1.5, scene_res * 1.5);
 camera.lookAt(new THREE.Vector3(scene_res / 2, 0, scene_res / 2));
 
 const scene = new THREE.Scene();
-
 scene_init(scene);
 
 function animate() {
-  requestAnimationFrame(animate);
+  stats.begin();
 
+  requestAnimationFrame(animate);
   scene_update(scene);
 
   renderer.render(scene, camera);
+  stats.end();
 };
+
+
+
+var gui = new dat.GUI({
+  height: 5 * 32 - 1
+});
+
+
+
+
+
+
+
+
 
 animate();
