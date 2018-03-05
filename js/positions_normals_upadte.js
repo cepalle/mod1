0b990a19493650addfabe972ae4552455014c7eb
Epalle <cyrille.epalle@epita.fr>
2018-03-05 13:20:39 +0100

    rename a variable

--- js/positions_normals_upadte.js
+++ js/positions_normals_upadte.js
@@ -34,15 +34,15 @@ function add_norm(normals, h, lpos) {
   normals[h++] = nz * 32767;
 }
 
-function positions_normals_upadte(positions, normals, U) {
+function positions_normals_upadte(positions, normals, M) {
   let k = 0;
   let h = 0;
   for (let i = 0; i < scene_res - 1; i++) {
     for (let j = 0; j < scene_res - 1; j++) {
-      let a = [i, U[i][j], j];
-      let b = [i + 1, U[i + 1][j], j];
-      let c = [i, U[i][j + 1], j + 1];
-      let d = [i + 1, U[i + 1][j + 1], j + 1];
+      let a = [i, M[i][j], j];
+      let b = [i + 1, M[i + 1][j], j];
+      let c = [i, M[i][j + 1], j + 1];
+      let d = [i + 1, M[i + 1][j + 1], j + 1];
 
       // positions
       add_pos(positions, k, [a, d, b]);
